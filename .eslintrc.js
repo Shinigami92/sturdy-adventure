a66dfc22db107108e39c0f23419cce7934413872
Shinigami92 <chrissi92@hotmail.de>
2022-09-19 09:22:23 +0200

    Init project base structure

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+// @ts-check
+const { defineConfig } = require('eslint-define-config');
+const { readGitignoreFiles } = require('eslint-gitignore');
+
+module.exports = defineConfig({
+  ignorePatterns: [...readGitignoreFiles(), '.eslintrc.js'],
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.lint.json',
+    sourceType: 'module',
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      { allowExpressions: true },
+    ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/lines-between-class-members': [
+      'warn',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+});
