d2d5f1ca2b0bec7bbe2fbb907b8bfa75bf9c5cd1
Shinigami92 <chrissi92@hotmail.de>
2022-09-19 12:20:52 +0200

    Init empty threejs scene

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,41 @@
-console.log('Hello World');
+import './main.css';
+
+import * as THREE from 'three';
+
+const container = document.getElementById('container');
+
+if (!container) {
+  throw new Error('Container not found');
+}
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
+container.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xbfe3dd);
+
+const camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight,
+  1,
+  100,
+);
+camera.position.set(0, 0, 10);
+
+window.onresize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+};
+
+function animate(): void {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+}
+
+animate();
