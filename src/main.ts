06351919be414e1ab3c99e63966dbf3233811373
Shinigami92 <chrissi92@hotmail.de>
2022-09-19 17:53:16 +0200

    Render a player and crosshair

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,6 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 container.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xbfe3dd);
 
 const camera = new THREE.PerspectiveCamera(
   40,
@@ -23,7 +22,30 @@ const camera = new THREE.PerspectiveCamera(
   1,
   100,
 );
-camera.position.set(0, 0, 10);
+camera.position.set(0, 0, 50);
+
+const ground = new THREE.Mesh(
+  new THREE.PlaneGeometry(100, 100),
+  new THREE.MeshBasicMaterial({ color: 0xbfe3dd }),
+);
+scene.add(ground);
+
+const player = new THREE.Mesh(
+  new THREE.PlaneGeometry(1, 2),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+);
+player.position.set(0, 0, 0);
+scene.add(player);
+
+const raycast = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+const crosshair = new THREE.Mesh(
+  new THREE.RingGeometry(0.4, 0.5, 12),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+);
+crosshair.position.set(0, 0, 0);
+scene.add(crosshair);
 
 window.onresize = () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -32,8 +54,24 @@ window.onresize = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 };
 
+function onMouseMove(event: MouseEvent): void {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  raycast.setFromCamera(mouse.clone(), camera);
+}
+
+container.addEventListener('mousemove', onMouseMove, false);
+
 function animate(): void {
   requestAnimationFrame(animate);
+
+  const intersections = raycast.intersectObjects(
+    scene.children.filter((a) => a.id === ground.id),
+  );
+  if (intersections.length > 0) {
+    const intersection = intersections[0];
+    crosshair.position.copy(intersection.point);
+  }
 
   renderer.render(scene, camera);
 }
