854d287a1d575f15b6e7f3ee66713ca6eafb8289
Shinigami92 <chrissi92@hotmail.de>
2022-10-03 12:13:17 +0200

    Update prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -8,4 +8,16 @@ module.exports = {
 
   singleQuote: true,
   trailingComma: 'all',
+
+  overrides: [
+    {
+      files: '*.json5',
+      options: {
+        parser: 'json5',
+        quoteProps: 'preserve',
+        singleQuote: false,
+        trailingComma: 'none',
+      },
+    },
+  ],
 };
